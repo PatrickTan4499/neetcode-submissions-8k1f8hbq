52c78c85283563e8050aa5a1fe6355b59d4957b5
Patrick Tan <patrick4499@gmail.com>
2026-04-12 22:18:00 -0700

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        return Math.max(dp(Arrays.copyOfRange(nums, 1, nums.length)), 
+        dp(Arrays.copyOfRange(nums, 0, nums.length-1)));
+    }
+
+    private int dp(int[] nums) {
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for(int n : nums) {
+            int newRob = Math.max(rob1 + n, rob2);
+            rob1 = rob2;
+            rob2 = newRob;
+            
+        }
+        return rob2;
+    }
+}
